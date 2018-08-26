55eda1e42e4ce1500e553da489b1910432aad07b
AbeIgnocious <k.l.kubat@gmail.com>
2018-08-26 12:15:29 +0200

    add example usage

--- waldorf_test.go
+++ waldorf_test.go
@@ -1,21 +1,15 @@
-# waldorf
-
-An easy to use library to validate data, for example incomming API requests.
-Waldorf checks everything that is wrong and returns a slice of complaints.
-
-```go
 package waldorf_test
 
 import (
 	"fmt"
 	"github.com/CIP-NL/waldorf"
-	)
+)
 
 func Example() {
 	w := waldorf.Observe()
 
 	for i := 0; i < 3; i++ {
-		w.ShouldBeTrue(i==0, "%d is one too many alread!", i)
+		w.ShouldBeTrue(i == 0, "%d is one too many alread!", i)
 	}
 	complaints := w.Ridicule()
 	fmt.Println(complaints.String())
@@ -23,4 +17,3 @@ func Example() {
 	// [1] is one too many alread!
 	// [2] is one too many alread!
 }
-```
